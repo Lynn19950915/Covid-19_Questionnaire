4ed118ecdde63e1a40c48bd8d77b036d210df75b
Lynn Chen <samuel19950915@gmail.com>
2021-11-05 11:35:31 +0800

    Update md-writing

--- 行政資料/200716  防疫點日記抽獎程式.py
+++ 行政資料/200716  防疫點日記抽獎程式.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 draw=[]
-with open("C:/Users/USER/Desktop/draw.csv") as csvfile:
+with open("C:/***/draw.csv") as csvfile:
     rows=csv.reader(csvfile)
     for row in rows:
         i=0
@@ -16,6 +16,6 @@ for j in range(3):
     if lucky_number not in winning:
         winning.append(lucky_number)
 
-print("第一獎中獎人:id#", winning[0])
-print("第二獎中獎人:id#", winning[1])
-print("第三獎中獎人:id#", winning[2])
+print("第一獎中獎人：id#", winning[0])
+print("第二獎中獎人：id#", winning[1])
+print("第三獎中獎人：id#", winning[2])
